f135ba4ca1f2b61de9572d22f4966dee94fb002b
l.s.dhillon6375 <l.s.dhillon6375@gmail.com>
2025-01-21 12:47:59 -0500

    Admin Login etc created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,7 +88,23 @@ const registerUser = async (req, res) => {
 
 
 const adminLogin = async (req, res) => {
-
+	try {
+		const {email, password} = req.body;
+		if (email === process.env.ADMIN_EMAIL  && password === process.env.ADMIN_PASSWORD) {
+			const token = jwt.sign(email+password, process.env.JWT_SECRET)
+      return res.json({ success: true, token });
+    }else{
+      return res.status(400).json({ success: false, message: 'Invalid credentials' });
+		}
+	} catch (error) {
+		console.log(error);
+		
+		return res.status(500).json({
+			success: false,
+			message: error.message,
+		});
+		
+	}
 };
 
 export { loginUser, registerUser, adminLogin }
